93dc6843f5b2552b8e8fe64c9f7431f676896a11
Ubaldo Aviles <2021171007@uteq.edu.mx>
2023-12-05 01:33:58 -0600

    integracion

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// app.mjs
-const cors = require("cors");
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const mainRoutes = require('./src/routes/mainRoutes.js');
+const path = require('path'); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 
 const corsOptions = {
@@ -19,7 +19,15 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', mainRoutes);
+app.use(mainRoutes);
+
+// Sirve los archivos estáticos de React desde la carpeta 'build'
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Ruta principal que sirve el archivo HTML de React
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
